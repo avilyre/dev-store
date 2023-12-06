7fd976ec67a2a6ee2d5c8e9820a294d28ad222ca
Avily Silva <avilyrs@gmail.com>
2023-12-06 15:45:49 -0300

    feat: home page

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,5 +1,71 @@
-export default async function Home() {
-  await new Promise(resolve => setTimeout(resolve, 2000));
+import Image from "next/image";
+import Link from "next/link";
 
-  return <h1>Home</h1>;
+export default async function Home() {
+  return (
+    <main className="grid grid-cols-9 grid-rows-6 gap-6 max-h-[860px]">
+      <Link
+        href="/"
+        className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-center"
+      >
+        <Image
+          className="group-hover:scale-105 transition-transform duration-500"
+          src="/moletom-ai-side.png"
+          height={860}
+          width={860}
+          quality={100}
+          alt=""
+        />
+
+        <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="text-sm truncate">Moleton IA Side</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$129,00
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        href="/"
+        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-center"
+      >
+        <Image
+          className="group-hover:scale-105 transition-transform duration-500"
+          src="/moletom-java.png"
+          height={860}
+          width={860}
+          quality={100}
+          alt=""
+        />
+
+        <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="text-sm truncate">Moleton IA Side</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$129,00
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        href="/"
+        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-center"
+      >
+        <Image
+          className="group-hover:scale-105 transition-transform duration-500"
+          src="/camiseta-dowhile-2022.png"
+          height={860}
+          width={860}
+          quality={100}
+          alt=""
+        />
+
+        <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="text-sm truncate">Moleton IA Side</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$129,00
+          </span>
+        </div>
+      </Link>
+    </main>
+  );
 }
