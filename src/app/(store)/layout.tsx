fa8bb9b9d037925cda44bd7b5de9c7e85a1098c1
Avily Silva <avilyrs@gmail.com>
2023-12-06 07:12:34 -0300

    feat: component

---
+++ src/app/(store)/layout.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../components/header";
+import { StoreLayoutProps } from "./interface";
+
+export default function StoreLayouts(props: StoreLayoutProps) {
+  const { children } = props;
+  return (
+    <div className="mx-auto grid  grid-rows-[min-content_max-content] min-h-screen w-full max-w-[1600px] gap-5 p-8">
+      <Header />
+      {children}
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} bg-zinc-950 text-zinc-50 antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
