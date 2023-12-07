451951c04f35022da6a4edeefdd40c3f2c56719e
Avily Silva <avilyrs@gmail.com>
2023-12-07 08:03:16 -0300

    feat: home page cache and skeleton

--- src/app/(store)/(home)/loading.tsx
+++ src/app/(store)/(home)/loading.tsx
@@ -1,3 +1,11 @@
+import { Skeleton } from "@/app/components/skeleton";
+
 export default function Loading() {
-  return <div>Loading</div>;
+  return (
+    <div className="grid grid-cols-9 grid-rows-6 gap-6 h-full">
+      <Skeleton className="col-span-6  row-span-6 h-[856px]" />
+      <Skeleton className="col-span-3  row-span-3" />
+      <Skeleton className="col-span-3  row-span-3" />
+    </div>
+  );
 }
--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -1,6 +1,8 @@
 import productsData from "../products.json";
 
 export async function GET() {
+  await new Promise(resolve => setTimeout(resolve, 2000));
+
   const featuredProducts = productsData.products.filter(
     product => product.featured,
   );
