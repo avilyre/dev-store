2f8df42197be69eb1b3cd98d3e7bc4c2419ade06
Avily Silva <avilyrs@gmail.com>
2023-12-09 07:45:37 -0300

    feat: caching featured products and improved fetch api function

--- src/data/api.ts
+++ src/data/api.ts
@@ -2,8 +2,14 @@ import { env } from "@/env";
 
 export function api(path: string, init?: RequestInit) {
   const baseURL = env.NEXT_PUBLIC_API_BASE_URL;
-  const prefix = "/api";
-  const url = new URL(prefix.concat(path), baseURL);
+  const prefix = "/api/";
+  const url = prefix.concat(path);
 
-  return fetch(url, init);
+  if (!url.startsWith("/api//")) {
+    throw new Error("The Request should be able started with '/' (bar)");
+  }
+
+  const buildedURL = new URL(url.replace("//", "/"), baseURL);
+
+  return fetch(buildedURL, init);
 }
