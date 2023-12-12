d77896681f966541e96e80b695670ffbcd220704
Avily Silva <avilyre@gmail.com>
2023-12-12 12:44:06 -0300

    feat: search products

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
-import { Search } from "lucide-react";
 import { CartWidget } from "./cart-widget";
+import { SearchForm } from "./search-form";
 
 export function Header() {
   return (
@@ -10,14 +10,8 @@ export function Header() {
         <Link href="/" className="text-2xl font-extrabold text-white">
           devstore
         </Link>
-        <form className="flex items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 w-[320px] ring-zinc-700">
-          <Search className="w-5 h-5 text-zinc-500" />
 
-          <input
-            placeholder="Buscar produtos..."
-            className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
-          />
-        </form>
+        <SearchForm />
       </div>
       <div className="flex items-center gap-4">
         <CartWidget />
