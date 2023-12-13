62b51602b372e22cc85fde6530c7708948bba018
Avily Silva <avilyre@gmail.com>
2023-12-13 16:00:04 -0300

    refactor: bff api removed

--- src/app/api/products/[slug]/interface.ts
+++
@@ -1,5 +0,0 @@
-export interface ParamsProps {
-  params: {
-    slug: string;
-  };
-}
--- src/app/api/products/route.ts
+++
@@ -1,5 +0,0 @@
-import productsData from "./products.json";
-
-export async function GET() {
-  return Response.json(productsData.products);
-}
