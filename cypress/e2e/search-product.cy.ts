5be4759372f28ec206d08e15ea5b63ba05217432
Avily Silva <avilyre@gmail.com>
2023-12-18 15:53:02 -0300

    test: search product

--- cypress/e2e/search-product.cy.ts
+++ cypress/e2e/search-product.cy.ts
@@ -1,6 +1,9 @@
 describe("Search for products", () => {
   it("should be able to type search form and submit it", () => {
-    cy.searchByQuery("ai side");
+    cy.visit("http://localhost:3000");
+
+    const searchForm = cy.get("input[class^='search-form']");
+    searchForm.type("moletom").parent().submit();
 
     cy.get('a[class^="product-item"]').should("exist");
   });
